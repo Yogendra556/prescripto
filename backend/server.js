c7bc726a10d7b72d9889113ad18a3220deb52ffe
Yogendra556 <yogendra9450sahu@gmail.com>
2025-10-31 14:19:52 +0530

    31_oct

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // Middlewares
 app.use(express.json())
 app.use(cors())
 
-// API Endpoint
+// API Endpoints
+app.use('/api/admin',adminRouter)
+
 app.get('/',(req,res)=>{
 res.send("API working")
 })
